23f22338d54eadb159c09838c477a5379b13cdd3
piaoqi13@qq.com <piaoqi13@qq.com>
2016-01-03 09:31:08 +0800

    登录信息实体类

---
+++ src/com/vengood/model/LoginInfo.java
@@ -0,0 +1,29 @@
+package com.vengood.model;
+
+/**
+ *类名：LoginInfo.java
+ *注释：登录实体类
+ *日期：2016年1月3日
+ *作者：王超
+ */
+public class LoginInfo {
+	private String name = null;
+	private String password = null;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
