b45564323d5071b0bb12cc2a22ca541fbd136be3
piaoqi13@qq.com <piaoqi13@qq.com>
2016-01-03 09:51:50 +0800

    调整包目录提示对话框

---
+++ src/com/vengood/dialog/TipDialog.java
@@ -0,0 +1,18 @@
+package com.vengood.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ *类名：TipDialog.java
+ *注释：提示对话框
+ *日期：2016年1月3日
+ *作者：王超
+ */
+public class TipDialog extends Dialog {
+
+	public TipDialog(Context context) {
+		super(context);
+	}
+	
+}
